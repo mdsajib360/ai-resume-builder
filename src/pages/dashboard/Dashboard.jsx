f23040d9c5568c1d354550c8142c6d109b4cd272
mdsajib360 <sajib360.dev@gmail.com>
2025-07-05 16:37:54 +0600

    download feature udpdate with html2canvas & jspdf package

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -26,20 +26,33 @@ function Dashboard() {
     <div className='p-10 md:px-20 lg:px-32'>
       <h2 className='font-bold text-3xl'>My Resume</h2>
       <p>Start Creating AI resume to your next Job role</p>
-      <div className='grid grid-cols-2 
-      md:grid-cols-3 lg:grid-cols-5 gap-5
-      mt-10
-      '>
-        <AddResume/>
-        {resumeList.length>0?resumeList.map((resume,index)=>(
-          <ResumeCardItem resume={resume} key={index} refreshData={GetResumesList} />
-        )):
-        [1,2,3,4].map((item,index)=>(
-          <div key={index} className='h-[280px] rounded-lg bg-slate-200 animate-pulse'>
-          </div>
-        ))
-        }
-      </div>
+   <div
+  className="
+    grid grid-cols-1
+    sm:grid-cols-2 
+    md:grid-cols-3 
+    lg:grid-cols-5 
+    gap-5 mt-10
+  "
+>
+  <AddResume />
+  
+  {resumeList.length > 0
+    ? resumeList.map((resume, index) => (
+        <ResumeCardItem
+          resume={resume}
+          key={index}
+          refreshData={GetResumesList}
+        />
+      ))
+    : [1, 2, 3, 4].map((item, index) => (
+        <div
+          key={index}
+          className="h-[280px] rounded-lg bg-slate-200 animate-pulse"
+        ></div>
+      ))}
+</div>
+
     </div>
   )
 }
